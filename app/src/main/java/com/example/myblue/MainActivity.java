3d49a50bd152213cca5fdba6f81d3df6a12c0f9c
klok101 <blackhawks4447@gmail.com>
2020-03-02 12:00:11 -0600

    First Commit

--- app/src/main/java/com/example/myblue/MainActivity.java
+++ app/src/main/java/com/example/myblue/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myblue;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
@@ -13,7 +15,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -23,16 +28,50 @@ public class MainActivity extends AppCompatActivity {
 
     BluetoothAdapter bluetoothAdapter;
     ArrayAdapter<String> arrayAdapter;
+    ArrayList<String> stringArrayList = new ArrayList<String>();
     ListView l1;
     ArrayList arrayList;
     Button enableButton;
+    Button scanButton;
+    Button scanButton2;
+    TextView scanText;
+    IntentFilter scanIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+    BroadcastReceiver scanModeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)) {
+                int modeValue = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR);
+                if (modeValue == BluetoothAdapter.SCAN_MODE_CONNECTABLE) {
+                    scanText.setText("The device is not in discoverable mode but can still receive connection");
+                } else if (modeValue == BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+                    scanText.setText("The device is in discoverable mode.");
+                } else if (modeValue == BluetoothAdapter.SCAN_MODE_NONE) {
+                    scanText.setText("The device is not in discoverable mode and can not receive connections");
+                } else {
+                    scanText.setText("Error");
+                }
+            }
+        }
+    };
+    BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                stringArrayList.add(device.getName());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        l1 = (ListView)findViewById(R.id.listView);
+        l1 = findViewById(R.id.listView);
         arrayList = new ArrayList();
 
         arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,arrayList);
@@ -40,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
         findPairedDevices();
 
-        enableButton = (Button) findViewById(R.id.enableButton);
+        enableButton = findViewById(R.id.enableButton);
         enableButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -49,10 +88,41 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        scanButton = findViewById(R.id.scanButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bluetoothAdapter.startDiscovery();
+            }
+        });
+
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(myReceiver, intentFilter);
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, stringArrayList);
+        l1.setAdapter(arrayAdapter);
+
+
+        //Enable button for 5 seconds
+        //Also changes the text view text
+        scanButton2 = findViewById(R.id.scan2);
+        scanText = findViewById(R.id.textView);
+
+        scanButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 5);
+                startActivity(discoverableIntent);
+            }
+        });
+        registerReceiver(scanModeReceiver, scanIntentFilter);
+
     }
 
     public void discoverDevices(View v){
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001);
         bluetoothAdapter.startDiscovery();
 
     }
